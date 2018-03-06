a8efd1dea10bc8ba9f0c674b1959917533c29cc0
bosstanayot <boss10520@hotmail.com>
2018-03-06 13:13:20 +0700

    add UnstructuredAddressModel
    - add UnstructuredAddressModel

---
+++ Trade Service Message Standard/src/java/UnstructuredAddressModel.java
@@ -0,0 +1,20 @@
+public class UnstructuredAddressModel {
+    String firstLineAddress;
+    String secondLineAddress;
+
+    public String getFirstLineAddress() {
+        return firstLineAddress;
+    }
+
+    public void setFirstLineAddress(String firstLineAddress) {
+        this.firstLineAddress = firstLineAddress;
+    }
+
+    public String getSecondLineAddress() {
+        return secondLineAddress;
+    }
+
+    public void setSecondLineAddress(String secondLineAddress) {
+        this.secondLineAddress = secondLineAddress;
+    }
+}
