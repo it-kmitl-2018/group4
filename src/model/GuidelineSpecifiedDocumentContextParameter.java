6fea585bbb584391d221f546a3b6c280ab7cf075
surasee2012 <doppelganger.2011.g@gmail.com>
2018-03-18 21:44:44 +0700

    Add GuidelineSpecifiedDocumentContextParameter Model and Test
    - Add GuidelineSpecifiedDocumentContextParameter model.
    - Add GuidelineSpecifiedDocumentContextParameterTest test.

---
+++ src/model/GuidelineSpecifiedDocumentContextParameter.java
@@ -0,0 +1,16 @@
+package model;
+
+public class GuidelineSpecifiedDocumentContextParameter {
+
+    //TODO : constructor, attribute, method because wait for requirement.
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
