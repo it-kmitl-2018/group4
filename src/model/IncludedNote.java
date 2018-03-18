79ff545b29cf49da04e5f660f872bf8ef32a1f26
surasee2012 <doppelganger.2011.g@gmail.com>
2018-03-18 20:00:04 +0700

    Add IncludedNote Model and Test
    - Create directory model.
    - Add IncludedNote model.
    - Create directory test.
    - Add IncludedNoteTest test.

---
+++ src/model/IncludedNote.java
@@ -0,0 +1,25 @@
+package model;
+
+public class IncludedNote {
+
+    //TODO : constructor, attribute, method because wait for requirement.
+
+    private String subject;
+    private String content;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
