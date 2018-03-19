8468ea4fc4a3175d74934f90f608a59e2e258758
intersignature <intersignature@hotmail.com>
2018-03-19 20:54:26 +0700

    add PostalTradeAddress Model and Test

---
+++ src/model/seller/PostalTradeAddress.java
@@ -0,0 +1,113 @@
+package model.seller;
+
+public class PostalTradeAddress {
+
+    String postCode;
+    String buildingName;
+    String buildingNumber;
+    String address;
+    String alley;
+    String villageName;
+    String villageNumber;
+    String streetName;
+    String cityName;
+    String citySubDivisionName;
+    String countryId;
+    String countrySubDivisionId;
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAlley() {
+        return alley;
+    }
+
+    public void setAlley(String alley) {
+        this.alley = alley;
+    }
+
+    public String getVillageName() {
+        return villageName;
+    }
+
+    public void setVillageName(String villageName) {
+        this.villageName = villageName;
+    }
+
+    public String getVillageNumber() {
+        return villageNumber;
+    }
+
+    public void setVillageNumber(String villageNumber) {
+        this.villageNumber = villageNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCitySubDivisionName() {
+        return citySubDivisionName;
+    }
+
+    public void setCitySubDivisionName(String citySubDivisionName) {
+        this.citySubDivisionName = citySubDivisionName;
+    }
+
+    public String getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountrySubDivisionId() {
+        return countrySubDivisionId;
+    }
+
+    public void setCountrySubDivisionId(String countrySubDivisionId) {
+        this.countrySubDivisionId = countrySubDivisionId;
+    }
+}
