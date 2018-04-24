6b6f11806f2721accd7c1a776a67a65dc1ecd1a3
BDLoneR <plumeria.in.sky@gmail.com>
2018-04-24 16:40:25 +0700

    delete old project and create project by Spring Initializr
    Fixed project and config Travis

---
+++ src/test/java/com/project/group4/Group4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.group4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Group4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
