00da38d63a15e3aa76ff9eb5c6f6bbba280ac74f
surasee2012 <doppelganger.2011.g@gmail.com>
2018-05-05 20:49:38 +0700

    Fix IdentifierJsonTest's Failure Test Case
    - Add setup method for setting variable's value.

--- src/test/java/com/project/group4/models/json/IdentifierJsonTest.java
+++ src/test/java/com/project/group4/models/json/IdentifierJsonTest.java
@@ -1,5 +1,6 @@
 package com.project.group4.models.json;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,6 +8,11 @@ import static org.junit.Assert.assertEquals;
 public class IdentifierJsonTest {
 
     private String id;
+
+    @Before
+    public void setup() {
+        this.id = "testID";
+    }
 
     @Test
     public void buildTest() {
