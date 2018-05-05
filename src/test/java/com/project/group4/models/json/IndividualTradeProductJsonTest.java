756d38ef61c7a7a6687d130155c301863a0af0d5
surasee2012 <doppelganger.2011.g@gmail.com>
2018-05-05 21:10:26 +0700

    Fix IndividualTradeProductJsonTest's Failure Test Case
    - Add setup method for setting variables' value.

--- src/test/java/com/project/group4/models/json/IndividualTradeProductJsonTest.java
+++ src/test/java/com/project/group4/models/json/IndividualTradeProductJsonTest.java
@@ -1,5 +1,6 @@
 package com.project.group4.models.json;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +9,12 @@ public class IndividualTradeProductJsonTest {
 
     private String batchId;
     private String expiryDateTime;
+
+    @Before
+    public void setup() {
+        this.batchId = "testBatchId";
+        this.expiryDateTime = "testExpiryDateTime";
+    }
 
     @Test
     public void buildTest() {
