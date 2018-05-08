bdbdff3863a0cbadbb794493f3d80bff84694f54
intersignature <intersignature@hotmail.com>
2018-05-09 03:09:13 +0700

    Add test method to AddressJsonTest and TradeContactJsonTest

--- src/test/java/com/project/group4/models/json/AddressJsonTest.java
+++ src/test/java/com/project/group4/models/json/AddressJsonTest.java
@@ -1,5 +1,7 @@
 package com.project.group4.models.json;
 
+import com.project.group4.converter.JsonConverter;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -19,6 +21,23 @@ public class AddressJsonTest {
     private String countryId;
     private String countrySubDivisionId;
     private String buildingNumber;
+
+    @Before
+    public void setup() {
+        postcodeCode = "postcode";
+        buildingName = "buildingName";
+        lineOne = "lineOne";
+        lineTwo = "lineTwo";
+        lineThree = "lineThree";
+        lineFour = "lineFour";
+        lineFive = "lineFive";
+        streetName = "streetName";
+        cityName = "cityName";
+        citySubDivisionName = "citySubDivisionName";
+        countryId = "countryId";
+        countrySubDivisionId = "countrySubDivisionId";
+        buildingNumber = "buildingNumber";
+    }
 
     @Test
     public void buildTest() {
@@ -51,5 +70,68 @@ public class AddressJsonTest {
         assertEquals(this.countryId, addressJson.countryId);
         assertEquals(this.countrySubDivisionId, addressJson.countrySubDivisionId);
         assertEquals(this.buildingNumber, addressJson.buildingNumber);
+    }
+
+    @Test
+    public void jsonStringTest() {
+        AddressJson addressJson = AddressJson.builder()
+                .postcodeCode(this.postcodeCode)
+                .buildingName(this.buildingName)
+                .lineOne(this.lineOne)
+                .lineTwo(this.lineTwo)
+                .lineThree(this.lineThree)
+                .lineFour(this.lineFour)
+                .lineFive(this.lineFive)
+                .streetName(this.streetName)
+                .cityName(this.cityName)
+                .citySubDivisionName(this.citySubDivisionName)
+                .countryId(this.countryId)
+                .countrySubDivisionId(this.countrySubDivisionId)
+                .buildingNumber(this.buildingNumber)
+                .build();
+
+        String expectedJson = "{\n" +
+                "    \"postcode_code\" : \"postcode\",\n" +
+                "    \"building_name\" : \"buildingName\",\n" +
+                "    \"line_one\" : \"lineOne\",\n" +
+                "    \"line_two\" : \"lineTwo\",\n" +
+                "    \"line_three\" : \"lineThree\",\n" +
+                "    \"line_four\" : \"lineFour\",\n" +
+                "    \"line_five\" : \"lineFive\",\n" +
+                "    \"street_name\" : \"streetName\",\n" +
+                "    \"city_name\" : \"cityName\",\n" +
+                "    \"city_sub_division_name\" : \"citySubDivisionName\",\n" +
+                "    \"country_id\" : \"countryId\",\n" +
+                "    \"country_sub_division_id\" : \"countrySubDivisionId\",\n" +
+                "    \"building_number\" : \"buildingNumber\"\n" +
+                "}";
+        String actualJson = JsonConverter.crateJsonString(addressJson);
+
+        assertEquals(expectedJson, actualJson);
+    }
+
+    @Test
+    public void jsonFileTest() {
+        AddressJson addressJson = AddressJson.builder()
+                .postcodeCode(this.postcodeCode)
+                .buildingName(this.buildingName)
+                .lineOne(this.lineOne)
+                .lineTwo(this.lineTwo)
+                .lineThree(this.lineThree)
+                .lineFour(this.lineFour)
+                .lineFive(this.lineFive)
+                .streetName(this.streetName)
+                .cityName(this.cityName)
+                .citySubDivisionName(this.citySubDivisionName)
+                .countryId(this.countryId)
+                .countrySubDivisionId(this.countrySubDivisionId)
+                .buildingNumber(this.buildingNumber)
+                .build();
+
+        Boolean expectedBool = true;
+        Boolean actualBool = JsonConverter.crateJsonFile(addressJson, "test");
+
+        assertEquals(expectedBool, actualBool);
+
     }
 }
--- src/test/java/com/project/group4/models/json/TradeContactJsonTest.java
+++ src/test/java/com/project/group4/models/json/TradeContactJsonTest.java
@@ -1,5 +1,7 @@
 package com.project.group4.models.json;
 
+import com.project.group4.converter.JsonConverter;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,6 +12,14 @@ public class TradeContactJsonTest {
     private String departmentName;
     private String uriId;
     private String telephoneNumber;
+
+    @Before
+    public void setup() {
+        personName = "personName";
+        departmentName = "departmentName";
+        uriId = "uriId";
+        telephoneNumber = "telephoneNumber";
+    }
 
     @Test
     public void buildTest() {
@@ -24,5 +34,40 @@ public class TradeContactJsonTest {
         assertEquals(this.departmentName, tradeContactJson.departmentName);
         assertEquals(this.uriId, tradeContactJson.uriId);
         assertEquals(this.telephoneNumber, tradeContactJson.telephoneNumber);
+    }
+
+    @Test
+    public void jsonStringTest() {
+        TradeContactJson tradeContactJson = TradeContactJson.builder()
+                .personName(this.personName)
+                .departmentName(this.departmentName)
+                .uriId(this.uriId)
+                .telephoneNumber(this.telephoneNumber)
+                .build();
+
+        String expectedJson = "{\n" +
+                "    \"person_name\" : \"personName\",\n" +
+                "    \"department_name\" : \"departmentName\",\n" +
+                "    \"uriid\" : \"uriId\",\n" +
+                "    \"complete_number\" : \"telephoneNumber\"\n" +
+                "}";
+        String actualJson = JsonConverter.crateJsonString(tradeContactJson);
+
+        assertEquals(expectedJson, actualJson);
+    }
+
+    @Test
+    public void jsonFileTest() {
+        TradeContactJson tradeContactJson = TradeContactJson.builder()
+                .personName(this.personName)
+                .departmentName(this.departmentName)
+                .uriId(this.uriId)
+                .telephoneNumber(this.telephoneNumber)
+                .build();
+
+        Boolean expectedBool = true;
+        Boolean actualBool = JsonConverter.crateJsonFile(tradeContactJson, "test");
+
+        assertEquals(expectedBool, actualBool);
     }
 }
